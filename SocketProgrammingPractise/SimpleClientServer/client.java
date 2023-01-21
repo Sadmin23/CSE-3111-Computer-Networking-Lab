f25c21c3142ce0d631f7aa277e519d1cbcc6ffac
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-21 17:26:03 +0600

    client server connection

--- SocketProgrammingPractise/SimpleClientServer/client.java
+++ SocketProgrammingPractise/SimpleClientServer/client.java
@@ -1,5 +1,17 @@
 package SocketProgrammingPractise.SimpleClientServer;
 
+import java.net.Socket;
+
 public class client {
 
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Client started");
+            Socket soc = new Socket("localhost", 9806);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- SocketProgrammingPractise/SimpleClientServer/server.java
+++ SocketProgrammingPractise/SimpleClientServer/server.java
@@ -1,5 +1,20 @@
 package SocketProgrammingPractise.SimpleClientServer;
 
-public class server {
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class server {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Waiting for clients...");
+            ServerSocket ss = new ServerSocket(9806);
+            Socket soc = ss.accept();
+            System.out.println("Connection established");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
