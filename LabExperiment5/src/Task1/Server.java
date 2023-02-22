957a3456bd87ba730ccacfeed6f0bfd666deb3f2
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-02-22 21:46:35 +0600

    server set

--- LabExperiment5/src/Task1/Server.java
+++ LabExperiment5/src/Task1/Server.java
@@ -1,4 +1,89 @@
 package Task1;
 
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+
 public class Server {
+    private static byte[] toHeader(int seqNum, int ackNum, int ack, int sf, int rwnd) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            outputStream.write(ByteBuffer.allocate(4).putInt(seqNum).array());
+            outputStream.write(ByteBuffer.allocate(4).putInt(ackNum).array());
+            outputStream.write(ByteBuffer.allocate(1).put((byte) ack).array());
+            outputStream.write(ByteBuffer.allocate(1).put((byte) sf).array());
+            outputStream.write(ByteBuffer.allocate(2).putShort((short) rwnd).array());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return outputStream.toByteArray();
+    }
+
+    private static int[] fromHeader(byte[] segment) {
+        int[] header = new int[5];
+        ByteBuffer buffer = ByteBuffer.wrap(segment);
+        header[0] = buffer.getInt();
+        header[1] = buffer.getInt();
+        header[2] = (int) buffer.get();
+        header[3] = (int) buffer.get();
+        header[4] = buffer.getShort();
+        return header;
+    }
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(8888);
+            Socket clientSocket = serverSocket.accept();
+            int recvBufferSize = 12;
+            int windowSize = 4 * recvBufferSize;
+            clientSocket.setReceiveBufferSize(recvBufferSize);
+            int expectedSeqNum = 0;
+            int ackNum = 0;
+            ByteArrayOutputStream receivedData = new ByteArrayOutputStream();
+
+            while (true) {
+                byte[] header = new byte[12];
+                clientSocket.getInputStream().read(header);
+                int[] headerFields = fromHeader(header);
+                int seqNum = headerFields[0];
+                int ackNumm = headerFields[1];
+                int ack = headerFields[2];
+                int sf = headerFields[3];
+                int rwnd = headerFields[4];
+                System.out.println(seqNum + " " + ackNum + " " + ack + " " + sf + " " + rwnd);
+
+                byte[] data = new byte[rwnd];
+                int bytesRead = clientSocket.getInputStream().read(data);
+                String str = new String(data, 0, bytesRead);
+                System.out.println(str);
+
+                if (bytesRead == -1) {
+                    break;
+                }
+
+                seqNum = ackNum;
+
+                if (seqNum == expectedSeqNum) {
+                    receivedData.write(data);
+                    ackNum += bytesRead;
+                    expectedSeqNum += bytesRead;
+
+                    byte[] toSendAck = toHeader(seqNum, ackNum, 1, 0, 12);
+                    clientSocket.getOutputStream().write(toSendAck);
+                } else {
+                    byte[] toSendAck = toHeader(seqNum, ackNum, 1, 0, 12);
+                    clientSocket.getOutputStream().write(toSendAck);
+                }
+            }
+
+            String receivedDataStr = receivedData.toString("UTF-8");
+            System.out.println(receivedDataStr);
+
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
