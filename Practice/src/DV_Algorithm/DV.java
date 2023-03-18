d61206dc67f6ec8e60a31c1b5ec1081ed2e0679f
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-03-18 22:52:29 +0600

    arrays copied

--- Practice/src/DV_Algorithm/DV.java
+++ Practice/src/DV_Algorithm/DV.java
@@ -4,7 +4,7 @@ public class DV {
 
     private static final int MAX = Integer.MAX_VALUE;
 
-    public boolean compareSubRouterays(int[][][] A, int[][][] B, int i1, int j1, int i2, int j2) {
+    public static boolean compareSubArrays(int[][][] A, int[][][] B, int i1, int j1, int i2, int j2) {
         for (int k = 0; k < A[0][0].length; k++) {
             if (A[i1][j1][k] != B[i2][j2][k]) {
                 return false;
@@ -13,13 +13,10 @@ public class DV {
         return true;
     }
 
-    public static boolean compareSubArrays(int[][][] A, int[][][] B, int i1, int j1, int i2, int j2) {
-        for (int k = 0; k < A[0][0].length; k++) {
-            if (A[i1][j1][k] != B[i2][j2][k]) {
-                return false;
-            }
+    public static void copyArrayt(int[][][] A, int[][][] B, int i1, int j1, int i2, int j2) {
+        for (int i = 0; i < A[i1][j1].length; i++) {
+            B[i2][j2][i] = A[i1][j1][i];
         }
-        return true;
     }
 
     public static void main(String[] args) {
@@ -33,7 +30,7 @@ public class DV {
         };
         Router[1] = new int[][] {
                 { MAX, MAX, MAX },
-                { 0, 2, 7 },
+                { 2, 0, 1 },
                 { MAX, MAX, MAX }
         };
         Router[2] = new int[][] {
@@ -43,6 +40,23 @@ public class DV {
         };
 
         System.out.println(compareSubArrays(Router, Router, 0, 0, 1, 1));
+
+        int flag = 1;
+
+        // while (true) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i != j) {
+                    if (compareSubArrays(Router, Router, j, i, i, i))
+                        continue;
+                    else {
+                        System.arraycopy(Router[i][i], 0, Router[j][i], 0, 3);
+                        flag = 0;
+                    }
+                }
+            }
+        }
+        // }
 
         for (int i = 0; i < Router.length; i++) {
             for (int j = 0; j < Router[i].length; j++) {
